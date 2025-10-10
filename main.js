5400a6b7f3ae231ff7462c7f17c7a51c84dcf4c4
aninhah25 <136709743+aninhah25@users.noreply.github.com>
2025-10-10 14:28:34 -0300

    ...

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
+for (let i =O,i<botoes.length;i++){
+    botoes [i].onclik = function()
+       for (let j = 0.j <botoes.length;j++)
+         botoes[j].classList. remove("ativo") 
+         textos[j].
+                
+            }
+         }
+}
